a1c5872ffdc5ba1deff9f909c265393b2da69bdb
toroflod <daniel.toro_flores@roche.com>
2020-05-05 11:57:55 +0200

    Handle project/project task not found exception

--- src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
+++ src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
@@ -1,8 +1,11 @@
 package io.agileintelligence.ppmtool.services;
 
 import io.agileintelligence.ppmtool.domain.Backlog;
+import io.agileintelligence.ppmtool.domain.Project;
 import io.agileintelligence.ppmtool.domain.ProjectTask;
+import io.agileintelligence.ppmtool.exceptions.ProjectNotFoundException;
 import io.agileintelligence.ppmtool.repositories.BacklogRepository;
+import io.agileintelligence.ppmtool.repositories.ProjectRepository;
 import io.agileintelligence.ppmtool.repositories.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,35 +19,50 @@ public class ProjectTaskService {
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
 
-    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
-        //PTs (projectTasks) to be added to a specific project != null, BL exists
-        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
-        //set the bl to pt
-        projectTask.setBacklog(backlog);
-        //we want our project sequence to be like this IDPRO-1 IDPRO-2 ..100 101
-        Integer BacklogSequence = backlog.getPTSequence();
+    @Autowired
+    private ProjectRepository projectRepository;
 
-        //Update the BL SEQUENCE
-        BacklogSequence++;
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) throws ProjectNotFoundException {
+        //TO DO
+        // Exceptions: Project not found
+        try {
+            //PTs (projectTasks) to be added to a specific project != null, BL exists
+            Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+            //set the bl to pt
+            projectTask.setBacklog(backlog);
+            //we want our project sequence to be like this IDPRO-1 IDPRO-2 ..100 101
+            Integer BacklogSequence = backlog.getPTSequence();
 
-        backlog.setPTSequence(BacklogSequence);
+            //Update the BL SEQUENCE
+            BacklogSequence++;
 
-        //Add sequence to Project Task
-        projectTask.setProjectSequence(projectIdentifier + "-" + BacklogSequence);
-        projectTask.setProjectIdentifier(projectIdentifier);
+            backlog.setPTSequence(BacklogSequence);
 
-        //INITIAL priority when priority null
-        if (projectTask.getPriority() == null) { // In the future we need projectTask.getPriority()==0  to handle the form
-            projectTask.setPriority(3);
+            //Add sequence to Project Task
+            projectTask.setProjectSequence(projectIdentifier + "-" + BacklogSequence);
+            projectTask.setProjectIdentifier(projectIdentifier);
+
+            //INITIAL priority when priority null
+            if (projectTask.getPriority() == null) { // In the future we need projectTask.getPriority()==0  to handle the form
+                projectTask.setPriority(3);
+            }
+            //INITIAL status when status is null
+            if (projectTask.getStatus() == "" || projectTask.getStatus() == null) {
+                projectTask.setStatus("TO_DO");
+            }
+            return projectTaskRepository.save(projectTask);
+        } catch (Exception e) {
+            throw new ProjectNotFoundException("Project not found");
         }
-        //INITIAL status when status is null
-        if (projectTask.getStatus() == "" || projectTask.getStatus() == null) {
-            projectTask.setStatus("TO_DO");
-        }
-        return projectTaskRepository.save(projectTask);
     }
 
     public Iterable<ProjectTask> findBackLogById(String id) {
+        Project project = projectRepository.findByProjectIdentifier(id);
+
+        if (project == null) {
+            throw new ProjectNotFoundException("Project with ID '" + id + "' does not exists");
+
+        }
         return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
     }
 }
