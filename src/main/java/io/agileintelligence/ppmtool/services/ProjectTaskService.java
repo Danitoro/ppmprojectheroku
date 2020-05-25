0bfa134d2bfa4dfd74181e37afc48e00c522ec63
toroflod <daniel.toro_flores@roche.com>
2020-05-25 11:17:09 +0200

    git updated after a problem with the project in intellij, branch updated

--- src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
+++ src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
@@ -68,8 +68,33 @@ public class ProjectTaskService {
 
     public ProjectTask findPTByProjectSequence(String backlog_id, String pt_id) {
 
+        Backlog backlog = backlogRepository.findByProjectIdentifier(backlog_id);
+        if(backlog==null){
+            throw new ProjectNotFoundException("Project with ID: '"+backlog_id+"' does not exist");
+        }
+        ProjectTask projectTask = projectTaskRepository.findByProjectSequence(pt_id);
+
+        if(projectTask == null){
+            throw new ProjectNotFoundException("Project Task '"+pt_id+"' not found");
+        }
+        if(!projectTask.getProjectIdentifier().equals(backlog_id)){
+            throw new ProjectNotFoundException("Project Task '"+pt_id+"' does not exist in project: '"+backlog_id);
+        }
         //make sure we are searching on the right backlog
 
-        return projectTaskRepository.findByProjectSequence(pt_id);
+        return projectTask;
+    }
+    public ProjectTask updateByProjectSequence(ProjectTask updatedTask, String backlog_id, String pt_id){
+        ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id);
+
+        projectTask = updatedTask;
+
+        return projectTaskRepository.save(projectTask);
+    }
+
+
+    public void deletePTByProjectSequence(String backlog_id, String pt_id){
+        ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id);
+        projectTaskRepository.delete(projectTask);
     }
 }
