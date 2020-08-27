c168e9bdaf759bfb160284df29a9d21f5518d465
toroflod <daniel.toro_flores@roche.com>
2020-08-27 14:52:24 +0200

    react added

---
+++ src/main/resources/static/precache-manifest.3152562a61b05bbd6d198b449d27dd0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65b7b3c38d2df776bd81d058c7d8cd22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89e328e25c4e51f5221a",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "4f166122c715d54f8899",
+    "url": "/static/css/main.86ded62d.chunk.css"
+  },
+  {
+    "revision": "89e328e25c4e51f5221a",
+    "url": "/static/js/2.de60c044.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.de60c044.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f166122c715d54f8899",
+    "url": "/static/js/main.a622305d.chunk.js"
+  },
+  {
+    "revision": "f0395242fcfe40790f57",
+    "url": "/static/js/runtime-main.85b40700.js"
+  }
+]);
